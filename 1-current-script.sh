5c90ee8b106256e94db4734a025e4786fe82beed
Ramu1706 <ram1706.devil@gmail.com>
2024-05-04 15:29:19 +0530

    expense shell

--- 1-current-script.sh
+++ 1-current-script.sh
@@ -5,8 +5,8 @@ COURSE="DevOps from Current Script"
 echo "Before calling other script, course: $COURSE"
 echo "Process ID of current shell script: $$"
 
- ./2-other-script.sh
+ #./2-other-script.sh
 
-#source ./16-other-script.sh
+source ./2-other-script.sh
 
 echo "After calling other script, course: $COURSE"
